1f7b7e48d6d6fc8b34b0b2c4ff0a89fc798f3c6b
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-02-01 21:54:33 +0100

    add user page

---
+++ my-app/src/containers/nameUser/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+const NameUser = () => {
+  return (
+    <div className="header">
+    <h1>Welcome back<br />Tony Jarvis!</h1>
+    <button className="edit-button">Edit Name</button>
+  </div>
+  );
+};
+
+export default NameUser;
