216ebfe092c72c5c0934a9c20b03c9bd6c1fc130
greskelll <jonattan44@hotmail.fr>
2023-05-31 13:50:07 +0200

    création d'un composant Banner

---
+++ argentbank-app/src/components/banner.js
@@ -0,0 +1,11 @@
+import './components.css';
+import { Motto } from './argentBankMotto';
+
+export function Banner() {
+	/* css class hero contain background img */
+	return (
+		<div class="hero">
+			<Motto />
+		</div>
+	);
+}
