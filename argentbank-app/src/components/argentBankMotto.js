2ecbdbbb70eb335797eb5d0164e49965bc82737a
greskelll <jonattan44@hotmail.fr>
2023-05-31 13:50:19 +0200

    Création d'une composant Motto

---
+++ argentbank-app/src/components/argentBankMotto.js
@@ -0,0 +1,13 @@
+import './components.css';
+
+export function Motto() {
+	return (
+		<section class="hero-content">
+			<h2 class="sr-only">Promoted Content</h2>
+			<p class="subtitle">No fees.</p>
+			<p class="subtitle">No minimum deposit.</p>
+			<p class="subtitle">High interest rates.</p>
+			<p class="text">Open a savings account with Argent Bank today!</p>
+		</section>
+	);
+}
