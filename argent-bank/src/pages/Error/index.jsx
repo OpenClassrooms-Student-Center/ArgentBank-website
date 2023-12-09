e7c8046d036d18dd42fba743ff5ac9860db2562e
Azunise <64028057+Azunise@users.noreply.github.com>
2023-12-09 01:58:46 +0100

    Delete excess directory

--- argent-bank/src/pages/Error/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Error() {
-   return (
-      <p>404</p>
-   )
-}
-
-export default Error
--- argent-bank/src/redux/selectors.js
+++
@@ -1,9 +0,0 @@
-export const selectLogin = (state) => state.login;
-
-export const selectCredentials = (state) => state.credentials;
-
-export const selectIsConnected = () => {
-	return (state) => state.login.isConnected;
-};
-
-export const selectUser = (state) => state.user;
