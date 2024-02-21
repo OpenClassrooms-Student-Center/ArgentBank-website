79cf2fdbff4ab1c025109919e742b76b505c2a10
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2024-02-21 13:15:46 +0100

    base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./assets/css/main.css";
 
 function App() {
   return (
